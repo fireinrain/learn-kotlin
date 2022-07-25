5987a48bd874273ed3fcedd9137d81c3b5942cac
sunrise <575563079@qq.com>
2022-07-25 11:56:19 +0800

    feat(add comments): add comments sample code

---
+++ src/main/kotlin/comments/Comments.kt
@@ -0,0 +1,20 @@
+package comments
+
+/**
+@Description:
+@Author  : fireinrain
+@Site    : https://github.com/fireinrain
+@File    : Comments
+@Software: IntelliJ IDEA
+@Time    : 2022/7/25 11:47 AM
+ */
+
+fun main(args: Array<String>) {
+    // 单行注释
+    /**
+     * 多行注释
+     */
+    var people:String = "baiabi" //行尾注释
+
+
+}
