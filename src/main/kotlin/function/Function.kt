c9efe768bf8d85d580365999e53d33dc22fc102e
sunrise <575563079@qq.com>
2022-07-25 18:58:48 +0800

    feat(add function): add function sample code

--- src/main/kotlin/function/Function.kt
+++ src/main/kotlin/function/Function.kt
@@ -12,9 +12,16 @@ package function
 fun main(args: Array<String>) {
     println("hello")
     printHelloToPeople("xiaoqian")
+    printInfo("xiaobai", 18)
+    var product = { a: Int -> println(a) }
+    product(10)
 }
 
 fun printHelloToPeople(name: String): String {
     println("hello $name")
     return "hello$name"
 }
+
+//如果函数返回单个表达式(如上例所示)，则可以省略函数体的花括号{}，并在 = 符号之后指定主体。
+
+fun printInfo(name: String, age: Int): String = "name is $name,age is $age"
